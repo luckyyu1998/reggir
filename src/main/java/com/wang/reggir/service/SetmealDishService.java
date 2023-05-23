b7eb5d885d5613710154faf52a30de83ef9e9d02
luckyyu1998-104 <528679852-104@qq.com>
2023-05-23 20:55:15 +0800

    reggir202305232

--- src/main/java/com/wang/reggir/service/SetmealDishService.java
+++
@@ -1,7 +0,0 @@
-package com.wang.reggir.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wang.reggir.pojo.SetmealDish;
-
-public interface SetmealDishService extends IService<SetmealDish> {
-}
--- src/main/java/com/wang/reggir/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.wang.reggir.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wang.reggir.pojo.User;
-
-public interface UserService extends IService<User> {
-
-}
